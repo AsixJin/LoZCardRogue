efa8e40d9409c5a8d75dad8c9fd1d1b6d9e3db5b
AsixJin <asixtrigger@gmail.com>
2015-05-08 00:36:06 -0500

    Chest Added

---
+++ Assets/Scripts/chest.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class chest : MonoBehaviour {
+
+	public Animator anim;
+	public bool beenOpened = false;
+	public ICard contents;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (beenOpened)
+		{
+			anim.SetBool("opened", true);
+		}
+	}
+}
